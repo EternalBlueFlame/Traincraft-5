58e7abc31aba9e5429dff403f7f516c9654121f3
CronosusCZ <cronosus@hotmail.com>
2017-09-17 02:55:46 +0200

    fix for tracks recipe
    wron item in tracks recipe
    iron ingot - > steelingot

--- src/main/java/train/common/core/handlers/RecipeHandler.java
+++ src/main/java/train/common/core/handlers/RecipeHandler.java
@@ -211,8 +211,8 @@ public class RecipeHandler {
 				TrainCraftingManager.instance.addRecipe(new ItemStack(ItemIDs.tcRailSmallRoadCrossing.item, 16),  "I I", "IPI", "I I", Character.valueOf('P'), new ItemStack(Blocks.stained_hardened_clay, 1, 15), Character.valueOf('I'), steelItem);
 				TrainCraftingManager.instance.addRecipe(new ItemStack(ItemIDs.tcRailSmallRoadCrossing1.item, 16),  "I I", "IPI", "I I", Character.valueOf('P'), new ItemStack(Blocks.stained_hardened_clay, 1, 7), Character.valueOf('I'), steelItem);
 				TrainCraftingManager.instance.addRecipe(new ItemStack(ItemIDs.tcRailSmallRoadCrossing2.item, 16),  "I I", "IPI", "I I", Character.valueOf('P'), new ItemStack(Blocks.stained_hardened_clay, 1, 3), Character.valueOf('I'), steelItem);
+				TrainCraftingManager.instance.addRecipe(new ItemStack(ItemIDs.tcRailSmallStraight.item, 16),  "I I", "IPI", "I I", Character.valueOf('P'), plankItem, Character.valueOf('I'), steelItem);// small straight track
 			}
-			TrainCraftingManager.instance.addRecipe(new ItemStack(ItemIDs.tcRailSmallStraight.item, 16),  "I I", "IPI", "I I", Character.valueOf('P'), plankItem, Character.valueOf('I'), Items.iron_ingot );// small straight track
 			for (ItemStack logStack :logs) {
 				/* Water Wheel */
 				TrainCraftingManager.instance.addRecipe(new ItemStack(BlockIDs.waterWheel.block, 1),  " P ", "PGP", " P ", Character.valueOf('P'), logStack,Character.valueOf('G'), ItemIDs.generator.item);
