57d03ac92ec5bc6243e794980fd72e8237e78c3e
Kenny Mensah <mensahkenny76@gmail.com>
2020-01-12 18:46:48 -0600

    Update GuiMTCInfo.java

--- src/main/java/train/client/gui/GuiMTCInfo.java
+++ src/main/java/train/client/gui/GuiMTCInfo.java
@@ -36,7 +36,7 @@ public class GuiMTCInfo extends GuiScreen {
         trainLevel.setMaxStringLength(1);
         trainLevel.setText(theLocomotive.trainLevel);
         trainID.setMaxStringLength(6);
-        trainID.setText(theLocomotive.getDataWatcher().getWatchableObjectString(23));
+        trainID.setText(theLocomotive.trainID);
         destination.setText(theLocomotive.getDestinationGUI());
 
 
