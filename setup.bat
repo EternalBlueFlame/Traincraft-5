28a38edf1d6a6a6555c851fe2f29dc8c6653d408
thedestroyer4312 <57116739+thedestroyer4312@users.noreply.github.com>
2026-08-21 00:18:10 -0700

    Modernize setup.sh and setup.bat

--- setup.bat
+++ setup.bat
@@ -1,114 +1,98 @@
 :restart
 @echo off
+cls
 @ECHO ##########################################################################
 @ECHO.
-@ECHO  Gradle startup script for Windows
+@ECHO  Gradle setup script for Windows
 @ECHO  This is to prepare the source for use with an IDE.
 @ECHO  Optionally you may compile the .jar as well.
-@ECHO  Be sure to install JDK 8 for your OS: https://www.oracle.com/technetwork/pt/java/javase/downloads/jdk8-downloads-2133151.html
-@ECHO  Also be sure to set it as your JAVA_HOME: https://docs.oracle.com/cd/E19182-01/820-7851/inst_cli_jdk_javahome_t/
-@ECHO. 
-@ECHO ##########################################################################
-
-@ECHO Choose an IDE:
-@ECHO [1] Eclipse - (Usually does not work)
-@ECHO [2] Idea (Intelij) - (Does work)
-@ECHO [3] Build the source.
-@echo off
 @ECHO.
-@ECHO. 
+@ECHO  Be sure to install JDK 8 for your OS.
+@ECHO  Also be sure to set it as your JAVA_HOME.
+@ECHO.
+@ECHO ##########################################################################
+@ECHO.
+
+@ECHO Choose an option:
+@ECHO  [1] Eclipse - (Usually does not work)
+@ECHO  [2] Idea (IntelliJ) - (Does work)
+@ECHO  [3] Build the source
+@ECHO.
+
 :tryagain
-set /p variable=""
-IF "%variable%"=="1" (goto eclipse)
-IF "%variable%"=="eclipse" (goto eclipse)
-IF "%variable%"=="Eclipse" (goto eclipse)
+set /p variable="Enter choice [1-3]: "
+IF "%variable%"=="1" goto eclipse
+IF /I "%variable%"=="eclipse" goto eclipse
 
-IF "%variable%"=="2" (goto intelij)
-IF "%variable%"=="Intelij" (goto intelij)
-IF "%variable%"=="intelij" (goto intelij)
-IF "%variable%"=="idea" (goto intelij)
-IF "%variable%"=="Idea" (goto intelij)
+IF "%variable%"=="2" goto intelij
+IF /I "%variable%"=="intelij" goto intelij
+IF /I "%variable%"=="intellij" goto intelij
+IF /I "%variable%"=="idea" goto intelij
 
-IF "%variable%"=="3" (goto build)
+IF "%variable%"=="3" goto build
+IF /I "%variable%"=="build" goto build
 
-pause
 @ECHO Incorrect option, try again.
+@ECHO.
 goto tryagain
 
 
-
 :eclipse
-@echo on
-start call gradlew setupDecompWorkspace --refresh-dependencies eclipse
-@echo off
 @ECHO.
+@ECHO Setting up workspace for Eclipse...
+call gradlew.bat setupDecompWorkspace --refresh-dependencies eclipse
+if errorlevel 1 goto fail
 @ECHO.
-@ ECHO After the other window finishes, close it and
-pause
-
 @ECHO ##########################################################################
 @ECHO.
 @ECHO  Mod is ready to be opened in Eclipse.
-@ECHO  You may open the project via gradle or directory import.
-@ECHO  NOTICE: if you get an error asking you to select a deobfuscateor, you can find this in:
-@ECHO  %USERPROFILE%/.gradle/caches/minecraft/net/minecraftforge/forge/FORGEVERSION/unpacked/conf
-@ECHO.
-@ECHO  You may now exit this window.
+@ECHO  If prompted for a deobfuscator, select:
+@ECHO  %%USERPROFILE%%/.gradle/caches/minecraft/net/minecraftforge/forge/1.7.10-10.13.4.1614-1.7.10/unpacked/conf
 @ECHO.
 @ECHO ##########################################################################
-
 pause
-
 goto quit
 
 
 :intelij
-@echo on
-start call gradlew setupDecompWorkspace --refresh-dependencies idea
-@echo off
 @ECHO.
-@ECHO.
-@ ECHO After the other window finishes, close it and
-pause
-
-@ECHO ##########################################################################
-@ECHO.
-@ECHO  Mod is ready to be opened in Intelij/Idea.
-@ECHO  You may open the project via gradle import or via: Traincraft-5.ipr
-@ECHO  NOTICE: if you get an error asking you to select a deobfuscateor, you can find this in:
-@ECHO  %USERPROFILE%/.gradle/caches/minecraft/net/minecraftforge/forge/FORGEVERSION/unpacked/conf
-@ECHO.
-@ECHO  You may now exit this window.
+@ECHO Setting up workspace for IntelliJ IDEA...
+call gradlew.bat setupDecompWorkspace --refresh-dependencies idea
+if errorlevel 1 goto fail
 @ECHO.
 @ECHO ##########################################################################
-
+@ECHO.
+@ECHO  Mod is ready to be opened in IntelliJ IDEA.
+@ECHO  Open via the generated Traincraft-5.ipr or directory import.
+@ECHO  If prompted for a deobfuscator, select:
+@ECHO  %%USERPROFILE%%/.gradle/caches/minecraft/net/minecraftforge/forge/1.7.10-10.13.4.1614-1.7.10/unpacked/conf
+@ECHO.
+@ECHO ##########################################################################
 pause
-
 goto quit
 
 
 :build
-@echo on
-start call gradlew setupDecompWorkspace --refresh-dependencies build
-@echo off
 @ECHO.
-@ECHO.
-@ ECHO After the other window finishes, close it and
-pause
-
-@ECHO ##########################################################################
-@ECHO.
-@ECHO  To find the compiled jar file look in /build/lib
-@ECHO.
-@ECHO  You may now exit this window, or continue to go back to the main menu.
+@ECHO Attempting to build jar file...
+call gradlew.bat setupDecompWorkspace --refresh-dependencies build
+if errorlevel 1 goto fail
 @ECHO.
 @ECHO ##########################################################################
-
+@ECHO.
+@ECHO  Built jar file can be found in build/libs/
+@ECHO.
+@ECHO ##########################################################################
 pause
+goto quit
 
-goto restart
+
+:fail
+@ECHO.
+@ECHO Gradle failed. See output above.
+pause
+exit /b 1
 
 
 :quit
-
 exit
